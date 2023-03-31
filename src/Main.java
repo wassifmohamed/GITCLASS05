8734056aff7d887982178b3286c835d0973a3142
wassifmohamed <wassifmohamed@gmail.com>
2023-03-31 17:16:39 -0400

    MyFirstCommit0001

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("00101001010");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
